215e8cef77e731df44e152a84270577ad24471bd
krish-thorcode <krish.thorocode@gmail.com>
2018-06-10 15:51:10 +0530

    put all the find_element_* statements inside try-except blocks

--- temp/temp.py
+++ temp/temp.py
@@ -22,13 +22,25 @@ except:
 browser.get(vtopbeta_elem.text)
 
 #6. Find the link to login page on vtopbeta captcha_img_elem and click on the elem to open the next page, ie, the login page
-login_page_link_elem = browser.find_element_by_css_selector('.btn.btn-primary.pull-right')
+try:
+    login_page_link_elem = browser.find_element_by_css_selector('.btn.btn-primary.pull-right')
+except:
+    print('Check the css selector for the button leading to the login page')
+    sys.exit()
 login_page_link_elem.click()
 
-username_elem = browser.find_element_by_css_selector('#uname')
-password_elem = browser.find_element_by_css_selector('#passwd')
-captcha_img_elem = browser.find_element_by_css_selector('img[alt = "vtopCaptcha"]')
+#7. From the login page, find the input elements (uname and pwd boxes and captcha box)
+try:
+    username_elem = browser.find_element_by_css_selector('#uname')
+    password_elem = browser.find_element_by_css_selector('#passwd')
+    captcha_elem = browser.find_element_by_css_selector()
+    captcha_img_elem = browser.find_element_by_css_selector('img[alt = "vtopCaptcha"]')
+except:
+    print('Input elements with the given css selectors were not found')
+    sys.exit()
 
-captcha_img_src = captcha_img_elem.get_attri
+#8. Find the image source of the captcha image
+captcha_img_src = captcha_img_elem.get_attribute('src')
+print(captcha_img_src)
 
 browser.quit()
