01f01609e0ee92385d99a4f283ed114fc1efa83e
krish-thorcode <krish.thorocode@gmail.com>
2018-06-13 10:25:09 +0530

    removed unnecessary lines of codes from scrap_vtop.py-2

--- scrap_vtop/vtopbeta_scrapping/scrap_vtop.py
+++ scrap_vtop/vtopbeta_scrapping/scrap_vtop.py
@@ -2,7 +2,6 @@
 import requests, sys, pytesseract, base64, getpass
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
